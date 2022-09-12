724da9443b260e83dd6890f05c147f67e0963c18
DARASSARAD <98292303+DARASSARAD@users.noreply.github.com>
2022-09-12 13:44:24 +0545

    Add files via upload

---
+++ Word Scramble Game/js/words.js
@@ -0,0 +1,94 @@
+let words = [
+    {
+        word: "division",
+        hint: "The process of dividing numbers"
+    },
+    {
+        word: "computer",
+        hint: "An electronic device that is used to make this program"
+    },
+    {
+        word: "mouse",
+        hint: "A synonym of Rat"
+    },
+    {
+        word: "exchange",
+        hint: "The act of trading"
+    },
+    {
+        word: "clothes",
+        hint: "Fabrics that we wear"
+    },
+    {
+        word: "ground",
+        hint: "Space for playing or meeting"
+    },
+    {
+        word: "latitide",
+        hint: "Longitude and ........"
+    },
+    {
+        word: "mountain",
+        hint: "Mt.Everest is a ..."
+    },
+    {
+        word: "happy",
+        hint: "A pleasant feeling of joy"
+    },
+    {
+        word: "brain",
+        hint: "The main organ of human body"
+    },
+    {
+        word: "expansion",
+        hint: "The process of increase or grow"
+    },
+    {
+        word: "country",
+        hint: "A politically identified region"
+    },
+    {
+        word: "group",
+        hint: "A number of objects or persons"
+    },
+    {
+        word: "taste",
+        hint: "Ability of tongue to detect flavour"
+    },
+    {
+        word: "store",
+        hint: "Large shop where goods are traded"
+    },
+    {
+        word: "field",
+        hint: "Area of land for farming activities"
+    },
+    {
+        word: "friend",
+        hint: "Person other than a family member"
+    },
+    {
+        word: "pocket",
+        hint: "A bag for carrying small items"
+    },
+    {
+        word: "needle",
+        hint: "A thin and sharp metal pin"
+    },
+    {
+        word: "stupid",
+        hint: "Person with low iq"
+    },
+    {
+        word: "speech",
+        hint: "A form of oral communication"
+    },
+    {
+        word: "minute",
+        hint: "One-sixtieth of an hour"
+    },
+    {
+        word: "library",
+        hint: "Place containing collection of books"
+    },
+]
